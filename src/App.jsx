69bee711d40f2ce0c9eebd9a8142de04f6531f25
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-01-27 14:53:55 +0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
-
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Hero } from "./components/Hero";
+import { Profile } from "./components/Profile";
+import { Projects } from "./components/Projects";
+import { Skills } from "./components/Skills";
 function App() {
   return (
     <>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ToastContainer } from "react-toastify";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+    <ToastContainer />
+  </BrowserRouter>
+);
