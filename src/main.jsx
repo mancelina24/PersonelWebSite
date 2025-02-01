40ef1398068a488723db68e8b56c525415914ba2
mancelina24 <feyza.turkay@ogr.iu.edu.tr>
2025-02-01 18:35:29 +0300

    14

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,6 @@
-import { createRoot } from "react-dom/client";
-
 import "./index.css";
 import App from "./App.jsx";
-
+import { createRoot } from "react-dom/client";
 import { ToastContainer } from "react-toastify";
 import { BrowserRouter } from "react-router-dom";
 import UserContextProvider from "./contexts/UserContext.jsx";
